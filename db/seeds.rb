23b68897a839a1443c27475d43360451dcd98391
Rashid Menhas <rashidmenhas10@gmail.com>
2023-02-11 10:33:38 +0500

    Set up association between model.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+
+first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+
+first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
+second_post = Post.create(author: first_user, title: 'sami', text: 'This is my second post')
+third_post = Post.create(author: first_user, title: 'How are', text: 'This is my third post')
+four_post = Post.create(author: first_user, title: 'you', text: 'This is my four post')
+
+Comment.create(post_id: first_post, author: first_user, text: 'Hi This is my first post on ruby on rails!' )
+Comment.create(post_id: third_post, author: first_user, text: 'Hi, Sami how are you?' )
+Comment.create(post_id: second_post, author: first_user, text: 'Hi I Hope you will be great' )
+Comment.create(post_id: first_post, author: first_user, text: 'Hi You create awesome portfolio' )
+Comment.create(post_id: four_post, author: first_user, text: 'Hi This is my First rails project!' )
+Comment.create(post_id: third_post, author: first_user, text: 'Thanks for everything' )
+
+
